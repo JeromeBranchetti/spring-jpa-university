0f85fb9afbf7c44ec78a343354eabeb044c25be9
JEROMEBRANCHETTI <jeromebranchetti00@gmail.com>
2022-01-10 08:03:29 +0100

    link ai dettagli

--- src/main/java/org/generation/italy/model/Degree.java
+++ src/main/java/org/generation/italy/model/Degree.java
@@ -1,46 +1,27 @@
 package org.generation.italy.model;
 
-import java.util.List;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
-import javax.persistence.OrderBy;
 import javax.persistence.Table;
 
 @Entity
-@Table(name="departments")
-public class Department {
+@Table(name="degrees")
+public class Degree {
 	
 	@Id
 	@Column(name="id")
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer id;
 	
 	private String name;
+	private String level;
 	private String address;
-	private String phone;
 	private String email;
 	private String website;
-	private String headOfDepartment;
 	
-	@OneToMany
-	@JoinColumn(name="department_id")
-	@OrderBy("name")
-	private List<Degree> degree;
-	
-	//Getters and Setters
-	public List<Degree> getDegree() {
-		return degree;
-	}
-	public void setDegree(List<Degree> degree) {
-		this.degree = degree;
-	}
 	
 	public Integer getId() {
 		return id;
@@ -54,17 +35,17 @@ public class Department {
 	public void setName(String name) {
 		this.name = name;
 	}
+	public String getLevel() {
+		return level;
+	}
+	public void setLevel(String level) {
+		this.level = level;
+	}
 	public String getAddress() {
 		return address;
 	}
 	public void setAddress(String address) {
 		this.address = address;
-	}
-	public String getPhone() {
-		return phone;
-	}
-	public void setPhone(String phone) {
-		this.phone = phone;
 	}
 	public String getEmail() {
 		return email;
@@ -78,10 +59,6 @@ public class Department {
 	public void setWebsite(String website) {
 		this.website = website;
 	}
-	public String getHeadOfDepartment() {
-		return headOfDepartment;
-	}
-	public void setHeadOfDepartment(String headOfDepartment) {
-		this.headOfDepartment = headOfDepartment;
-	}
+	
+
 }
